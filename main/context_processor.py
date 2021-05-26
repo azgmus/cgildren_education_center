088f0be34522533259e0d22466119c2f4e872e1c
azgmus <herodotus@inbox.ru>
2021-05-27 01:11:32 +0300

    fucking commit

--- main/context_processor.py
+++ main/context_processor.py
@@ -3,6 +3,9 @@ from main.models import GeneralInfo, InfoPages
 
 def extras(request):
     general = GeneralInfo.objects.get(id=1)
+    #general = {'name': 'da'}
     info_pages = InfoPages.objects.values('name')
+    #info_pages = ['da']
 
     return {'general': general, 'info_pages': info_pages}
+
