a7291a2f56e88cf4a10eace38800bf9c9a9a2056
azgmus <herodotus@inbox.ru>
2021-05-26 22:53:28 +0300

    here goes nothing

--- children_education_center/settings.py
+++ children_education_center/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
