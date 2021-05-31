7b914c4d2465cf048617854f22f9eb73d720428f
azgmus <herodotus@inbox.ru>
2021-05-31 03:50:38 +0300

    )

--- children_education_center/settings.py
+++ children_education_center/settings.py
@@ -60,6 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+
+    # for static in prod
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'children_education_center.urls'
